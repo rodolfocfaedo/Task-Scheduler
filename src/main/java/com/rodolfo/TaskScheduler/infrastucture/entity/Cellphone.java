e5b7be6b533d08796a744b3f28c10084554b34a8
Rodolfo Faedo <faedorodolfo.programming@gmail.com>
2025-02-04 12:11:13 -0300

    First commit

---
+++ src/main/java/com/rodolfo/TaskScheduler/infrastucture/entity/Cellphone.java
@@ -0,0 +1,27 @@
+package com.rodolfo.TaskScheduler.infrastucture.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "cellphone")
+public class Cellphone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "areaCode", length = 3)
+    private String areaCode;
+
+    @Column(name = "number", length = 9)
+    private String number;
+
+    @Column(name = "person_id")
+    private Long person_id;
+}
---
+++ src/main/java/com/rodolfo/TaskScheduler/infrastucture/entity/Person.java
@@ -0,0 +1,51 @@
+package com.rodolfo.TaskScheduler.infrastucture.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
+@Builder
+public class Person implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id" ,referencedColumnName = "id")
+    private List<Cellphone> cellphone;
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
